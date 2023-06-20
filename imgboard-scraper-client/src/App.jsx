802a6d0d1f1c2952a7b0a8f1c41d948ebb6a478e
donwung <113479299+donwung@users.noreply.github.com>
2023-06-20 14:52:48 -0700

    created react page

---
+++ imgboard-scraper-client/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [newLink, setNewLink] = useState("")
+    const [imgLinks, setImgLinks] = useState([])
+
+    const handleOnGet = () => {
+        axios.get(`http://127.0.0.1:8000/goto-link?URL=${newLink}`)
+            .then((res) => {
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }
+
+    const handleOnPost = (e) => {
+        e.preventDefault();
+        axios.post(`http://127.0.0.1:8000/post-link`, {
+            "gotoURL": newLink
+        }).then((res) => {
+            console.log(res)
+            setImgLinks(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <a href={newLink}><button onClick={() => handleOnGet()}>GOTO</button></a>
+            <a href={newLink}><button onClick={(e) => handleOnPost(e)}>POST</button></a>
+            <input type="text" onChange={(e) => setNewLink(e.target.value)}></input>
+            <div>
+                <p>imglinks</p>
+                <img src="https://i.4cdn.org/v/1687281098418245.png"></img>
+                {/* {imgLinks.map((imgLink) => {
+                    return <img src={imgLink} crossOrigin="anonymous"></img>
+                })} */}
+            </div>
+        </div>
+    )
+}
+
+export default App
