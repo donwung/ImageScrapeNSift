b10279cf008aa1ed965a7cdef62f2b930d941b13
donwung <113479299+donwung@users.noreply.github.com>
2023-06-21 15:26:31 -0700

    crudely introduced multithreading

--- server/4chscraper_copy.py
+++ server/4chscraper_copy.py
@@ -7,6 +7,7 @@ import re
 import os
 import urllib.request
 from os import walk
+import threading
 import shutil
 import sys
 
@@ -40,12 +41,16 @@ def parse_url(thread_URL):
     return links
 
 # downloads file into downloads folder
+
+
 def download(url, file_name):
     with open("./../imgboard-scraper-client/public/download/"+file_name, "wb") as file:
         res = get(url)
         file.write(res.content)
 
 # after getting a regex match array, this flattens an array of characters
+
+
 def flattenPostNumArr(arr):
     res = ""
     for i in range(1, len(arr)):
@@ -53,15 +58,9 @@ def flattenPostNumArr(arr):
     return res
 
 
-@app.post("/post-link")
-async def post_link(payload: dict = Body(...)):
-    allImgs = []
-    URL_to_scrape = payload["gotoURL"]
-
-    links = parse_url(URL_to_scrape)
-    for link in links:
-        _link = "https:"+link.get("href")
-        # print(_link)
+def dl_img_at_indices(links, all_imgs, increment, start):
+    for link_i in range(start - 1, len(links), increment):
+        _link = "https:"+links[link_i].get("href")
 
         postNum = re.findall("[0123456789]", _link)
         postNum = flattenPostNumArr(postNum)
@@ -70,18 +69,81 @@ async def post_link(payload: dict = Body(...)):
         fileExt = fileExt[1]
 
         oneFile = [postNum, fileExt]
-        allImgs.append(oneFile)
+        all_imgs.append(oneFile)
         fileName = postNum + fileExt
 
         # this downloads one file named as the post number
-        if not os.path.exists(f"./../imgboard-scraper-client/public/download/{fileName}"):
+        if not os.path.exists(f"./static/{fileName}"):
             download(_link, fileName)
+    return all_imgs
+
+
+@app.post("/post-link")
+async def post_link(payload: dict = Body(...)):
+    URL_to_scrape = payload["gotoURL"]
+    all_imgs = []
+    number_of_threads = 8
+    links = parse_url(URL_to_scrape)
+
+    t1 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 1))
+    t2 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 2))
+    t3 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 3))
+    t4 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 4))
+    t5 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 5))
+    t6 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 6))
+    t7 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 7))
+    t8 = threading.Thread(
+        target=dl_img_at_indices, args=(links, all_imgs, number_of_threads, 8))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+    t7.start()
+    t8.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+    t6.join()
+    t7.join()
+    t8.join()
+
+    # for link in links:
+    #     _link = "https:"+link.get("href")
+    #     # print(_link)
+
+    #     postNum = re.findall("[0123456789]", _link)
+    #     postNum = flattenPostNumArr(postNum)
+
+    #     fileExt = re.findall("\.[a-z]+", _link)
+    #     fileExt = fileExt[1]
+
+    #     oneFile = [postNum, fileExt]
+    #     allImgs.append(oneFile)
+    #     fileName = postNum + fileExt
+
+    #     # this downloads one file named as the post number
+    #     if not os.path.exists(f"./../imgboard-scraper-client/public/download/{fileName}"):
+    #         download(_link, fileName)
     return
 
 
 @app.get("/show-folder")
 async def show_folder():
     print("def show-folder")
-    filenames = next(walk("./../imgboard-scraper-client/public/download/"), (None, None, []))[2]  # [] if no file
+    filenames = next(walk("./../imgboard-scraper-client/public/download/"),
+                     (None, None, []))[2]  # [] if no file
     # print(filenames)
     return filenames
