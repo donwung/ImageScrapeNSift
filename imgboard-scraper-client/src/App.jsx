6b6f975d9c71cf211c8c5af0e46cbca591566e3d
donwung <113479299+donwung@users.noreply.github.com>
2023-06-20 19:00:26 -0700

    can now display images

--- imgboard-scraper-client/src/App.jsx
+++ imgboard-scraper-client/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
     const [count, setCount] = useState(0)
     const [newLink, setNewLink] = useState("")
-    const [imgLinks, setImgLinks] = useState([])
+    const [imgs, setImgs] = useState([])
 
     const handleOnGet = () => {
         axios.get(`http://127.0.0.1:8000/goto-link?URL=${newLink}`)
@@ -35,7 +35,8 @@ function App() {
         e.preventDefault();
         axios.get(`http://127.0.0.1:8000/show-folder`)
             .then((res) => {
-                console.log(res)
+                console.log(res.data)
+                setImgs(res.data)
             }).catch((err) => {
                 console.log(err)
             })
@@ -47,12 +48,18 @@ function App() {
             <button onClick={(e) => handleOnPost(e)}>POST</button>
             <button onClick={(e) => handleOnShowFolder(e)}>show folder</button>
             <input type="text" style={{ width: "500px" }} onChange={(e) => setNewLink(e.target.value)}></input>
-            <div>
-                <p>imglinks</p>
-                <img src="https://i.4cdn.org/v/1687281098418245.png"></img>
-                {/* {imgLinks.map((imgLink) => {
-                    return <img src={imgLink} crossOrigin="anonymous"></img>
-                })} */}
+            <div style={{width: "500px"}}>
+                <p>imgs</p>
+                {/* <img style={{width: "500px"}}src={testimg}></img> */}
+                {imgs.map((img) => {
+                    return (
+                        <div>
+                            {img}
+                            {/* <img src={testimg}></img> */}
+                            <img style={{width: "500px"}} src={"/download/"+img}></img>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
