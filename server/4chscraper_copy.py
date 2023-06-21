6d1176122e0f692ae808966e7de1b4acfdd094b1
donwung <113479299+donwung@users.noreply.github.com>
2023-06-20 18:09:54 -0700

    copied 4chscraper code to use with fastapi

---
+++ server/4chscraper_copy.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+from fastapi import FastAPI, Request, Body
+from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, render_template, redirect, request
+from requests import get
+import re
+import os
+import urllib.request
+import shutil
+import sys
+
+
+app = FastAPI()
+
+####### get around CORS ####
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+############################
+
+
+# reads url, puts it into soup, returns all hrefs ie web files
+def parse_url(thread_URL):
+    req = urllib.request.Request(
+        thread_URL, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(req)
+    html_doc = response.read()
+    # print(response)
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    links = soup.find_all("a", class_="fileThumb")
+    return links
+
+# downloads file into static folder
+
+
+def download(url, file_name):
+    with open("./download/"+file_name, "wb") as file:
+        res = get(url)
+        file.write(res.content)
+
+# after getting a regex match array, this flattens an array of characters
+
+
+def flattenPostNumArr(arr):
+    res = ""
+    for i in range(1, len(arr)):
+        res = res + arr[i]
+    return res
+
+
+@app.post("/post-link")
+async def post_link(payload: dict = Body(...)):
+    allImgs = []
+    URL_to_scrape = payload["gotoURL"]
+
+    links = parse_url(URL_to_scrape)
+    for link in links:
+        _link = "https:"+link.get("href")
+        # print(_link)
+
+        postNum = re.findall("[0123456789]", _link)
+        postNum = flattenPostNumArr(postNum)
+
+        fileExt = re.findall("\.[a-z]+", _link)
+        fileExt = fileExt[1]
+
+        oneFile = [postNum, fileExt]
+        allImgs.append(oneFile)
+        fileName = postNum + fileExt
+
+        # this downloads one file named as the post number
+        if not os.path.exists(f"./download/{fileName}"):
+            download(_link, fileName)
+    return
+
+
+@app.get("/show-folder")
+def show_folder():
+    print("def show-folder")
+    filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
+    return ("returned")
--- server/main.py
+++ server/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request, Body
 from fastapi.middleware.cors import CORSMiddleware
+from requests import get
 import urllib.request
+import re
 from bs4 import BeautifulSoup
+import os
+from os import listdir
+from os.path import isfile, join
 
 
 app = FastAPI()
@@ -33,24 +38,27 @@ async def post_link(payload: dict = Body(...)):
     URL_to_scrape = payload["gotoURL"]
     if (URL_to_scrape != ""):
         img_links = get_image_links(URL_to_scrape)
-        print(img_links)
+        # download_imgs_from_links(img_links)
         return img_links
     else:
         return "URL required"
 
-#TODO: snag hrefs from that reference to media
+# TODO: snag hrefs from that reference to media
+
+
 def get_image_links(URL: str):
     all_img_links = []
     html_doc = read_html_response(URL)
     soup = BeautifulSoup(html_doc, 'html.parser')
-    links = soup.find_all("a", class_="fileThumb")
+    links = soup.find_all("a")
+    # print(soup)
+    # print(links)
+    # print(links)
     for link in links:
         _link = "https:"+link.get("href")
-        # print(_link)
-        all_img_links.append(_link)
-    # I have all the image links
-    # print(all_img_links)
-    # now to save the links into a folder
+        if re.search(".jpg$|.png$", _link):
+            print(_link)
+            all_img_links.append(_link)
     return all_img_links
 
 
@@ -60,3 +68,20 @@ def read_html_response(URL: str):
     response = urllib.request.urlopen(req)
     html_doc = response.read()
     return html_doc
+
+
+def download_imgs_from_links(links):
+    i = 0
+    for link in links:
+        i += 1
+        ext = re.findall("\.[a-z]+$", link)[0]
+        # print(ext)
+        print("mediacontent"+str(i)+ext)
+        file_name = "mediacontent_"+str(i)+ext
+        download(link, file_name)
+
+
+def download(url, file_name):
+    with open("./download/"+file_name, "wb") as file:
+        res = get(url)
+        file.write(res.content)
